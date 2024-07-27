6f5f5ac5489ae02201429e6a7c79c237be27cd3c
SanaGithubUber <sanagithububer@gmail.com>
2024-07-27 09:23:16 +0400

    Emp_task

---
+++ Models/Employee_task_table.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace emp_task.Models
+{
+    public class Employee_task_table
+    {
+        public int Id { get; set; }
+        [Key] 
+        public int Profile_id { get; set; }
+        [ForeignKey(nameof(Profile_id))]
+        public string Task_Name { get; set; }
+        public string Task_Descr { get; set; }
+        public DateTimeOffset Start_time { get; set; }
+        public int Status { get; set; }
+         
+
+
+
+    }
+}
